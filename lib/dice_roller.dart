d2a49984b75767c911a4d3b767592789c938bbf6
EwaBaczynska <baczynska.ewa@gmail.com>
2024-02-24 19:26:45 +0100

    roll_dice_project

---
+++ lib/dice_roller.dart
@@ -0,0 +1,47 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+final randomizer = Random();
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+
+  @override
+  State<DiceRoller> createState() {
+    return _DiceRollerState();
+  }
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  var currentDiceRoll = 2;
+
+  void rollDice() {
+    setState(() {
+      currentDiceRoll = randomizer.nextInt(4) + 1;
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset('assets/images/dice-$currentDiceRoll.png', width: 200),
+        //const SizedBox(
+        //  height: 10,
+        //),
+        TextButton(
+          onPressed: rollDice,
+          style: TextButton.styleFrom(
+            padding: const EdgeInsets.all(
+              30,
+            ),
+            foregroundColor: Colors.white,
+            textStyle: const TextStyle(fontSize: 50),
+          ),
+          child: const Text('Roll Dice'),
+        ),
+      ],
+    );
+  }
+}
